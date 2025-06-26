a8a1911d22529b832749b26c3b23c4b719b87057
Jayaprakash Das <jayprakashdas5763@gmail.com>
2025-06-26 17:59:37 +0530

    delete one photo and change in footer and navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
       <header className='header flex'>
         <div className='logoDiv'>
           <a href='/' className='logo flex'>
-            <h1><MdOutlineTravelExplore className='icon' />Travel.</h1>
+            <h1><MdOutlineTravelExplore className='icon' />Book My Trip</h1>
           </a>
         </div>
 
